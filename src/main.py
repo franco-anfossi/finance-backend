c96aff6e888e186731de4f5f8483526fdc969b95
Franco Anfossi <franco.anfossi@uc.cl>
2024-11-22 01:04:17 -0300

    feat(routes): add transaction routes to the main application

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from .auth.routes import router as auth_router
 from .bank.routes import router as bank_router
 from .middleware.cors import setup_cors
+from .transaction.routes import router as transaction_router
 from .user.routes import router as user_router
 
 app = FastAPI()
@@ -12,6 +13,7 @@ setup_cors(app)
 app.include_router(auth_router, prefix="/api")
 app.include_router(bank_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
+app.include_router(transaction_router, prefix="/api")
 
 
 @app.get("/")
